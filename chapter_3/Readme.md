b0aa6c9f3eb7fdf9bf0563b1d005dc50a977f43d
David Mowrer <david.mowrer@comcast.net>
2014-10-10 12:18:48 -0600

    LRTHW Chapter_3 / Initial commit

---
+++ chapter_3/Readme.md
@@ -0,0 +1,11 @@
+Extra credit questions:
+1) Please make these changes in ex3.rb
+2) Create a new file called irb_calculator.md. Run 'irb' in your console, do some 
+   calculator stuff and then copy/paste your screen into irb_calculator.md. 
+   Bonus points for using markdown syntax(1) to make it look good.
+3) Create a new Ruby file for this one
+4) Add your research to readme.md
+5) Copy ex3.rb to ex3_floating.rb for this one.
+
+
+
---
+++ chapter_3/ex3.rb
@@ -0,0 +1,37 @@
+# Prints out the text I will now count my chickens
+puts 'I will now count my chickens'
+
+# Prints out Hens with the calculated value for number of chickens.
+puts "Hens #{25 + 30 / 6}"
+# Prints out Roosters with the calculated value for the number of roosters.
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# Prints out the text Now I will count my eggs:
+puts 'Now I will count the eggs:'
+
+# Calculates the number of eggs.
+puts 3 + 2 + 1 - 5  + 4 % 2 - 1 / 4 + 6
+
+# Prints out the text Is it true that 3 + 2 < 5 - 7?
+puts 'Is it true that 3 + 2  < 5 - 7?'
+
+# Prints out the value of true or false.
+puts 3 + 2 < 5 - 7
+
+# Prints out What is 3 + 2? and the calculated value.
+puts "What is 3 + 2? #{3 + 2}"
+# Prints out What is 5 - 7? and the calculated value.
+puts "What is 5 - 7? #{5 - 7}"
+
+# Prints out the text Oh, that's why it's false.
+puts "Oh, that's why it's false."
+
+# Pints out the text How about some more.
+puts 'How about some more.'
+
+# Prints out Is it greater? and the value true or false.
+puts "Is it greater? #{5 > -2}"
+# Prints out Is it greater or equal? and the value true or false.
+puts "Is it greater or equal? #{5 >= -2}"
+# prints out Is it less or equal? and  the value true or false.
+puts "Is it less or equal? #{5 <= -2}"
